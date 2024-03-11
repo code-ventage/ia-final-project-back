774f0314acb50f6c04e5a2965298e744a6156286
jldestive <jldestive2@gmail.com>
2024-03-11 19:35:02 -0400

    fix: bug del response data

--- python-final/services/userService.py
+++ python-final/services/userService.py
@@ -24,6 +24,7 @@ class UserService(Service):
     def index(username: str = None) -> list:  
         try:
             response = UserRepository.index()
+            
             if username is None:
                 Service.response['response']['data'] = response
             else:
