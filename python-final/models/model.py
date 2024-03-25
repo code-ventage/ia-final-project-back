3a75a11298db03e76c64653ae87f1ba1d8f65894
Victor Palmero <117776704+palmerovicdev@users.noreply.github.com>
2024-03-25 10:39:33 -0400

    docs: add docstring for models in python module

--- python-final/models/model.py
+++ python-final/models/model.py
@@ -1,6 +1,16 @@
 from abc import ABC
 class Model(ABC):
+    """
+     Clase abstracta Model.
+
+     """
     def toJson(self) -> dict:
+        """
+        toJson(self) -> dict:
+             Convierte los atributos del objeto a un diccionario.
+
+        :return: el diccionario resultante
+        """
         attributes = [attr for attr in dir(self) if not callable(getattr(self, attr)) and not attr.startswith("__")]
         response = {}
         for i in range(1, len(attributes)):
@@ -8,5 +18,11 @@ class Model(ABC):
         return response
     
     def reverseJson(self, json: dict) -> None:
+        """
+        reverseJson(self, json: dict) -> None:
+             Establece los atributos del objeto a partir de un diccionario.
+        :param json:
+        :return: void
+        """
         for k, v in json.items():
             setattr(self, k, v)
--- python-final/models/userModel.py
+++ python-final/models/userModel.py
@@ -7,12 +7,26 @@ sys.path.append(dir_father)
 from models.model import Model
 
 class userModel(Model):
+    """
+    Clase userModel que hereda de Model.
+
+    Atributos:
+        _username (str): Nombre de usuario.
+        _password (str): Contraseña del usuario.
+    """
     def __init__(self, username: str = None, password: str =  None) -> None:
         super().__init__()
         self._username = username
         self._password = password
         
     def __eq__(self, __value: object) -> bool:
+        """
+        __eq__(self, __value: object) -> bool:
+            Compara si el objeto proporcionado es igual a este objeto.
+
+        :param __value:
+        :return: boolean
+        """
         if type(__value) == userModel :
             if __value.username == self.username:
                 return True
@@ -21,16 +35,41 @@ class userModel(Model):
         
     @property
     def username(self) -> str:
+        """
+        username(self) -> str:
+            Obtiene el nombre de usuario.
+
+        :return: el nombre del usuario
+        """
         return self._username
     @username.setter
     def username(self, username: str) -> None:
+        """
+         username(self, username: str) -> None:
+            Establece el nombre de usuario.
+
+        :param username:
+        :return: void
+        """
         self._username = username
         
     @property
     def password(self) -> str:
+        """
+           password(self) -> str:
+            Obtiene la contraseña del usuario.
+
+        :return: la contraseña del usuario
+        """
         return self._password
     @password.setter
     def password(self, password: str) -> None:
+        """
+          password(self, password: str) -> None:
+            Establece la contraseña del usuario.
+        :param password:
+        :return: void
+        """
         self._password = password
 
 if '__main__' == __name__:
@@ -38,6 +77,3 @@ if '__main__' == __name__:
     print(j.toJson())
     i = userModel()
     i.reverseJson(j)
-
-    
-    
