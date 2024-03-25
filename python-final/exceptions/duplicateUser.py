5658fd882b78f421c98f1d27ce3d56d526ccabba
Victor Palmero <117776704+palmerovicdev@users.noreply.github.com>
2024-03-25 10:39:25 -0400

    docs: add docstring for exceptions in python module

--- python-final/exceptions/duplicateUser.py
+++ python-final/exceptions/duplicateUser.py
@@ -1,4 +1,9 @@
 class DuplicateUser(Exception):
+    """
+    Exepcion lanzada al encontrar usuarios duplicados.
+
+    Atributos:
+        message (str): Explicacion del error
+    """
     def __init__(self, message: str = 'Ya existe este usuario') -> None:
         super().__init__(self, message)
-    
