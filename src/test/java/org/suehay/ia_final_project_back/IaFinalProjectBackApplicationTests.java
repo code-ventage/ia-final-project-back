f407bd1f9521a431d24f4f28766d8b4faceb582f
Victor Palmero <117776704+palmerovicdev@users.noreply.github.com>
2024-04-05 09:53:44 -0400

    test: created test for consult repository

--- src/test/java/org/suehay/ia_final_project_back/IaFinalProjectBackApplicationTests.java
+++ src/test/java/org/suehay/ia_final_project_back/IaFinalProjectBackApplicationTests.java
@@ -1,24 +1,47 @@
 package org.suehay.ia_final_project_back;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.suehay.ia_final_project_back.model.request.UserRequest;
-import org.suehay.ia_final_project_back.repository.UserRepositoryImpl;
-import org.suehay.ia_final_project_back.repository.UserScoreRepositoryImpl;
+import org.suehay.ia_final_project_back.repository.ConsultRepository;
+import org.suehay.ia_final_project_back.repository.UserRepository;
+import org.suehay.ia_final_project_back.repository.UserScoreRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@RequiredArgsConstructor
 class IaFinalProjectBackApplicationTests {
     @Autowired
-    UserRepositoryImpl userRepository;
+    UserRepository userRepository;
 
     @Autowired
-    UserScoreRepositoryImpl userScoreRepository;
+    UserScoreRepository userScoreRepository;
+
+    @Autowired
+    ConsultRepository consultRepository;
 
     @Test
-    void contextLoads() {
-        userScoreRepository.index();
-        userRepository.login(new UserRequest("suehay", "1234"));
+    void consultRepositoryDigit() {
+        // ---------- From letters -----------------
+        assertThat(consultRepository.makeDigitConsultToProlog("cero")).isEqualTo("N = 0");
+        assertThat(consultRepository.makeDigitConsultToProlog("uno")).isEqualTo("N = 1");
+        assertThat(consultRepository.makeDigitConsultToProlog("mil doscientos treinta y cuatro")).isEqualTo("N = 1234");
+        assertThat(consultRepository.makeDigitConsultToProlog("doscientos un mil trescientos veinticinco")).isEqualTo("N = 201325");
+        assertThat(consultRepository.makeDigitConsultToProlog("ciento veintiun millones trescientos un mil doscientos")).isEqualTo("N = 121301200");
+        assertThat(consultRepository.makeDigitConsultToProlog("ciento un millones trescientos un mil doscientos")).isEqualTo("N = 101301200");
+    }
+
+    @Test
+    void consultRepositoryLetter(){
+        // ---------- From digits -----------------
+        assertThat(consultRepository.makeLetterConsultToProlog("0")).isEqualTo("N = cero");
+        assertThat(consultRepository.makeLetterConsultToProlog("1")).isEqualTo("N = uno");
+        assertThat(consultRepository.makeLetterConsultToProlog("1234")).isEqualTo("N = mil doscientos treinta y cuatro");
+        assertThat(consultRepository.makeLetterConsultToProlog("201325")).isEqualTo("N = doscientos un mil trescientos veinticinco");
+        assertThat(consultRepository.makeLetterConsultToProlog("121301200")).isEqualTo("N = ciento veintiun millones trescientos un mil doscientos");
+        assertThat(consultRepository.makeLetterConsultToProlog("101301200")).isEqualTo("N = ciento un millones trescientos un mil doscientos");
     }
 
 }
